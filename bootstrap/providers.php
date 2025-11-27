a49a5696eead3010c16e3453ee795b4590e6e885
wahyudedik <wdedyk@gmail.com>
2025-11-28 04:12:52 +0700

    modified:   bootstrap/providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+    // Telescope disabled in production
+    // App\Providers\TelescopeServiceProvider::class,
 ];
