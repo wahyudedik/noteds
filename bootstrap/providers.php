07934d6f42eab89f9ef29fea486b6d9223b6040e
wahyudedik <wdedyk@gmail.com>
2025-12-07 00:04:12 +0700

    modified:   bootstrap/providers.php 	modified:   composer.json

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,12 @@ return [
     App\Providers\AppServiceProvider::class,
     // Telescope disabled in production
     // App\Providers\TelescopeServiceProvider::class,
+
+    // Register Debugbar only in development
+    ...match (app()->environment()) {
+        'local' => [
+            \Barryvdh\Debugbar\ServiceProvider::class,
+        ],
+        default => [],
+    },
 ];
