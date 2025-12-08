ecd9f3161bddaf76f9cc0ae6362882861fa75ca0
wahyudedik <wdedyk@gmail.com>
2025-12-09 05:04:22 +0700

    Fix: use guest layout instead of guest-layout component

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,6 @@
-<x-guest-layout>
+@extends('layouts.guest')
+
+@section('content')
     <div class="space-y-6">
         <div class="text-center">
             <h2 class="text-3xl font-semibold text-slate-900">{{ __('messages.log_in_title') }}</h2>
@@ -93,4 +95,4 @@
             @endif
         </div>
     </div>
-</x-guest-layout>
+@endsection
--- storage/framework/views/0317170da5dd4daaf02cfd1a4250e990.php
+++ storage/framework/views/0317170da5dd4daaf02cfd1a4250e990.php
@@ -1,13 +1,4 @@
-<?php if (isset($component)) { $__componentOriginal69dc84650370d1d4dc1b42d016d7226b = $component; } ?>
-<?php if (isset($attributes)) { $__attributesOriginal69dc84650370d1d4dc1b42d016d7226b = $attributes; } ?>
-<?php $component = App\View\Components\GuestLayout::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
-<?php $component->withName('guest-layout'); ?>
-<?php if ($component->shouldRender()): ?>
-<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
-<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
-<?php $attributes = $attributes->except(\App\View\Components\GuestLayout::ignoredParameterNames()); ?>
-<?php endif; ?>
-<?php $component->withAttributes([]); ?>
+<?php $__env->startSection('content'); ?>
     <div class="space-y-6">
         <div class="text-center">
             <h2 class="text-3xl font-semibold text-slate-900"><?php echo e(__('messages.log_in_title')); ?></h2>
@@ -257,14 +248,6 @@
             <?php endif; ?>
         </div>
     </div>
- <?php echo $__env->renderComponent(); ?>
-<?php endif; ?>
-<?php if (isset($__attributesOriginal69dc84650370d1d4dc1b42d016d7226b)): ?>
-<?php $attributes = $__attributesOriginal69dc84650370d1d4dc1b42d016d7226b; ?>
-<?php unset($__attributesOriginal69dc84650370d1d4dc1b42d016d7226b); ?>
-<?php endif; ?>
-<?php if (isset($__componentOriginal69dc84650370d1d4dc1b42d016d7226b)): ?>
-<?php $component = $__componentOriginal69dc84650370d1d4dc1b42d016d7226b; ?>
-<?php unset($__componentOriginal69dc84650370d1d4dc1b42d016d7226b); ?>
-<?php endif; ?>
-<?php /**PATH D:\PROJECT\LARAVEL\noteds\resources\views/auth/login.blade.php ENDPATH**/ ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.guest', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH D:\PROJECT\LARAVEL\noteds\resources\views/auth/login.blade.php ENDPATH**/ ?>
