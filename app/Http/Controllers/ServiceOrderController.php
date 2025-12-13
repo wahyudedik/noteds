27939b5c1226521f12b4d2b4f56f2ce3c0c8386d
wahyudedik <wdedyk@gmail.com>
2025-12-13 11:52:39 +0700

    fix: add missing DB import to ServiceOrderController

--- app/Http/Controllers/ServiceOrderController.php
+++ app/Http/Controllers/ServiceOrderController.php
@@ -6,6 +6,7 @@ use App\Models\ServiceOrder;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class ServiceOrderController extends Controller
